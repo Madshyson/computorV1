d45f9e5d2b2ec6d02d590f6da86404b98c55a7d8
Martin Lavanant <mlavanan@student.42.fr>
2018-02-12 06:33:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ discriminant = require('./Ressources/discriminant.js');
 resolver = require('./Ressources/resolver.js');
 
 parser.parser(process.argv,  function (parsed) {
-    console.log("--The eqution is : --");
+    console.log("--The equation is : --");
     console.log(process.argv[2]);
     console.log("Reduced formula is :");
     console.log(parsed.a + 'X2 +' + parsed.b + 'X + ' + parsed.c + '  = 0');
